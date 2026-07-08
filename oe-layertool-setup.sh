varigit/oe-layersetup

commit 93b05920ada625b2655a8e23b31eaecc0e54ec57

oe-layertool-setup: Enable vex and spdx 3.0 for Yocto SDK for AM62x
Enable vex and spdx 3.0 in TI Yocto SDK build setups by appending the required INHERIT and
extra exclusions include to conf/local.conf.

Co-authored-by: Alifer Moraes <alifer.m@variscite.com>
Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

oe-layertool-setup.sh
@@ -840,6 +840,13 @@ EOM
         } >> "$confdir/local.conf"
         rm "$oebase/tmp_append_local.conf"
     fi
+
+    # Enable vex and spx 3.0
+    echo "" >> $confdir/local.conf
+    echo '# Enable vex and spx 3.0' >> $confdir/local.conf
+    echo 'INHERIT += "vex"' >> $confdir/local.conf
+    echo 'INHERIT:remove = "create-spdx"' >> $confdir/local.conf
+    echo 'INHERIT += "create-spdx-3.0"' >> $confdir/local.conf
 }
 
 print_motd() {

commit 741b87d36d5985eba7cb9073c298829e60c0c0b7

oe-layertool-setup: only enable vex and spdx 3.0 when they exist
VEX and SPDX 3.0 generation require Yocto classes that only exist in
relatively new repositories.

While these classes have been backported to Scarthgap, some Scarthgap
branches do not have them yet. Enable VEX and SPDX 3.0 generation only
when the relevant classes exist.

Fixes: 93b0592 ("oe-layertool-setup: enable VEX and SPDX 3.0
generation")
Signed-off-by: Matthias Proske <matthias.p@variscite.com>

oe-layertool-setup.sh
@@ -847,12 +847,17 @@ EOM
         rm "$oebase/tmp_append_local.conf"
     fi
 
-    # Enable vex and spx 3.0
-    echo "" >> $confdir/local.conf
-    echo '# Enable vex and spx 3.0' >> $confdir/local.conf
-    echo 'INHERIT += "vex"' >> $confdir/local.conf
-    echo 'INHERIT:remove = "create-spdx"' >> $confdir/local.conf
-    echo 'INHERIT += "create-spdx-3.0"' >> $confdir/local.conf
+    # Enable vex and spdx 3.0 if available
+    # This should be in oe-core/meta/classes, but maybe this changes in the future
+    if find "$sourcedir" -name "vex.bbclass" -print -quit | grep -q .; then
+        if find "$sourcedir" -name "create-spdx-3.0.bbclass" -print -quit | grep -q .; then
+            echo "" >> $confdir/local.conf
+            echo '# Enable vex and spdx 3.0' >> $confdir/local.conf
+            echo 'INHERIT += "vex"' >> $confdir/local.conf
+            echo 'INHERIT:remove = "create-spdx"' >> $confdir/local.conf
+            echo 'INHERIT += "create-spdx-3.0"' >> $confdir/local.conf
+        fi
+    fi
 }
 
 print_motd() {